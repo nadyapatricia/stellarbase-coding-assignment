8fe0e7f72ccae44298ac7f78495f662141954941
“Nadya <nadyapatricia21@gmail.com>
2024-12-14 17:57:44 +0700

    Change single quote to double quote and add semicolon

--- components/providers/tanstack-provider.tsx
+++ components/providers/tanstack-provider.tsx
@@ -1,16 +1,16 @@
-'use client'
+"use client";
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { useState } from 'react'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
 
 interface TanstackProviderProps {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
 export const TanstackProvider = ({ children }: TanstackProviderProps) => {
-  const [queryClient] = useState(() => new QueryClient())
+  const [queryClient] = useState(() => new QueryClient());
 
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-  )
+  );
 }
